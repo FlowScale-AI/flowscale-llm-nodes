9525ea9560c78671cc5a6aead80b8a02cab8e4ab
rupayan <rupayan@insynkstudios.com>
2025-01-08 15:02:58 +0530

    Add JSON Extract Property node to NODE_CLASS_MAPPINGS and NODE_DISPLAY_NAME_MAPPINGS

--- __init__.py
+++ __init__.py
@@ -5,11 +5,13 @@ from .nodes.embedding.openai import OpenAIEmbedding
 from .nodes.vectordb.astradb import AstraDBStoreEmbeddingsNode
 
 from .utilitynodes.fileloader import FileLoaderNode
+from .utilitynodes.json_extracter import ExtractPropertyNode
 
 NODE_CLASS_MAPPINGS = {
   "openai": OpenAIAPI,
   "openai_embedding": OpenAIEmbedding,
   "astradb_store_embeddings": AstraDBStoreEmbeddingsNode,
+  "json_extract_property": ExtractPropertyNode,
   
   "file_loader": FileLoaderNode
 }
@@ -19,5 +21,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
   "openai_embedding": "[FS] OpenAI Embedding",
   "astradb_store_embeddings": "[FS] AstraDB Store Embeddings",
   
-  "file_loader": "[FS] File Loader"
+  "file_loader": "[FS] File Loader",
+  "json_extract_property": "[FS] JSON Extract Property"
 }
